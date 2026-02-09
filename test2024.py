5cceca5c676f9f948f194730edaab9997c9f52ac
yishay ossi <yishayossi10@gmail.com>
2026-02-10 01:20:04 +0200

    2

--- test2024.py
+++ test2024.py
@@ -12,26 +12,62 @@ import random
 # randomNum()
 
 
-2
-def checkString():
-    bool = True
-    while(bool):
-      countA = 0
-      countDouble = 0
-      string = input("give me a string: ")
+# 2
+# def checkString():
+#     bool = True
+#     while(bool):
+#       countA = 0
+#       countDouble = 0
+#       string = input("give me a string: ")
 
-      if string[0] == string[-1]:
-        bool = False
+#       if string[0] == string[-1]:
+#         bool = False
 
-      for str in string:
-         if str == "A":
-            countA+=1
-      print("Count of A:", countA)    
+#       for str in string:
+#          if str == "A":
+#             countA+=1
+#       print("Count of A:", countA)    
       
-      if len(string)%2 == 0:
-         countDouble+=1
-    print("Count of double string:", countDouble)
+#       if len(string)%2 == 0:
+#          countDouble+=1
+#     print("Count of double string:", countDouble)
 
 
-checkString()
+# checkString()
 
+3
+def is_balanced(arr):
+   listEven = 0
+   listOdd = 0
+   for i, num in enumerate(arr):
+      if i%2==0:
+         listEven+=num
+      else:
+         listOdd+=num
+   if listEven == listOdd:
+      return True
+   else:
+      return False
+
+
+listi = [12, 3, 7, 15, 5, 12, 6]
+check_arr = is_balanced(listi)
+print(check_arr)
+
+def fill_return(size, x, y):
+   if size > 2:
+      listOfNum = []
+      for i in range(size-1):
+         num = random.randint(x,y)
+         listOfNum.append(num)
+         
+      listEven = 0
+      listOdd = 0
+      for i, num in enumerate(listOfNum):
+        if i%2==0:
+          listEven+=num
+        else:
+          listOdd+=num
+   print(listEven, listOdd)
+      
+fill_return(7,3,9)
